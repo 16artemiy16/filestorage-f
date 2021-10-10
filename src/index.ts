3c0c0194918439ccb0e946f014ae5eb845227159
Artem Polikarpov <artemiypolikarpov@gmail.com>
2021-10-10 15:36:40 +0300

    Init mongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,30 @@
 import Fastify from 'fastify';
 import { config } from 'dotenv';
 import allRoutes from './routes';
+import { MongoClient } from 'mongodb';
 
 config();
 
-export const { APP_PORT } = process.env as Record<string, string | number>;
+const {
+  APP_PORT,
+  MONGODB_URI
+} = process.env as Record<string, string | number>;
 
 const fastify = Fastify();
 
 fastify.register(allRoutes);
 
-fastify.listen(APP_PORT, (err, address) => {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-  console.log(`Server listening at ${address}`);
-})
+const mongoClient = new MongoClient(MONGODB_URI as string);
+
+mongoClient.connect().then(() => {
+  fastify.listen(APP_PORT, (err, address) => {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+  });
+}).catch((err) => {
+  console.log(err);
+  process.exit(1);
+});
